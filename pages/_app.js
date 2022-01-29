0f3bf45c8ef890789589a58f0b774895cd9e843a
Luuan11 <luancraft55@gmail.com>
2022-01-29 19:19:48 -0300

    ajuste de tela

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,6 @@ function GlobalStyle() {
     return (
       <style global jsx>
         {`
-@import url("https://fonts.googleapis.com/css2?family=Poppins&display=swap"
 
           * {
             margin: 0;
@@ -11,19 +10,25 @@ function GlobalStyle() {
             list-style: none;
           }
 
+          @import url("https://fonts.googleapis.com/css2?family=Poppins&display=swap"
+
           body {
             font-family: "Poppins", "Arial", sans-serif;
           }
 
-          ::-webkit-scrollbar-track {
-          background: #232424;
-          border-radius: 10px;
-          }
-          
-          ::-webkit-scrollbar-thumb {
-          background: #763DE1;
-          border-radius: 10px;
-          }
+          ::-webkit-scrollbar {
+            width: 10px;
+            }
+
+            ::-webkit-scrollbar-track {
+            background: #232424;
+            border-radius: 10px;
+            }
+            
+            ::-webkit-scrollbar-thumb {
+            background: #763DE1;
+            border-radius: 10px;
+            }
 
           /* App fit Height */
           html,
